720861af10c0f869b7f3fa89beeb884d19e94d65
Desenvolvedor <masterdfn@gmail.com>
2015-01-31 09:04:28 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,53 @@
-<?
-echo teste;
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- Colocando acentuação no pagina --!>
+
+<?php 
+ini_set('default_charset','UTF-8'); 
 ?>
+
+<!-- Fim Colocando acentuação na pagina --!>
+
+<!-- Puxando o arquivo de CSS --!>
+<link rel="stylesheet" type="text/css" href="css/hp.css">
+<style>
+body{
+background: #b5bdc8; /* Old browsers */
+
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#b5bdc8), color-stop(45%,#828c95), color-stop(100%,#28343b));
+}
+</style>
+
+<!-- Fim da Pagina de CSS --!>
+
+
+<body class="body">
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>
+<table border="1" class=tabela2 ><tr><td bgcolor="fcfcfc">
+<center><u><h1>GESTÃO</h1></u></center>
+<center>
+<form method="post" action="login.php">
+<table><tr><td>
+<label>Usuário:</label></td>
+<td>
+<input type="text" name="usuario" maxlength="50" /></td></tr>
+<tr><td>
+<label>Senha:</label></td>
+<td>
+<input type="password" name="senha" maxlength="50" />
+</td></tr><tr><td>
+<input type="submit" value="Entrar" />
+</td></tr>
+</table>
+</form>
+</center>
+</td>
+</tr>
+</table>
+</center>
+</body>
